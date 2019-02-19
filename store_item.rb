f3be47a22040f43cd2885dd0485724ae99909226
“SIeep” <Brandon_n_evans@live.com>
2019-02-18 20:56:19 -0600

    add four items to shop using hashes

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+beef_stick = {height: 12, circumference: 1, color: "beef color", flavor: "mild", price: 2}
+
+seaweed_chips = {number_of_chips: 20, color: "green", taste: "yummy", price: 3}
+
+fried_pickles = {number_of_pickles: 10, color: "fried", taste: "yummy with ranch", price: "tree fiddy"}
+
+milk_juice = {:size => "one gallon", :color => "white/orange", :price => 2.25}
+
+# puts beef_stick[:color]
+
+# puts milk_juice[:price]
