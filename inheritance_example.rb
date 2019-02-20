b2103d1d575d81e3074c6336d905b6362fc77a7d
“SIeep” <Brandon_n_evans@live.com>
2019-02-19 21:27:57 -0600

    manager.rb WIP

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,34 +1,40 @@
-class Methods
+class Vehicle 
   def initialize
-      @speed = 0
-      @direction = 'north'
-    end
+    @speed = 0
+    @direction = 'north'
+  end
 
-    def brake
-      @speed = 0
-    end
+  def brake
+    @speed = 0
+  end
 
-    def accelerate
-      @speed += 10
-    end
+  def accelerate
+    @speed += 10
+  end
 
-    def turn(new_direction)
-      @direction = new_direction
-    end
-
-    def honk_horn
-      puts "Beeeeeeep!"
-    end
+  def turn(new_direction)
+    @direction = new_direction
+  end
 end
 
-class Car < Methods
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
 end
 
-class Bike  < Methods
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
 end
 
 car = Car.new
 p car
+car.accelerate
+p car
+p car.honk_horn
 
-bike = Bike.new
-p bike
+# bike = Bike.new
+# p bike.accelerate
+# p bike.ring_bell
